d19ddaeb60a9cbc72d0b003067400f757f0bd9d0
Chung Thế Hào <chungthehao@gmail.com>
2019-04-06 14:05:42 +0700

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,31 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>(); // It will hold some Recipe data
 
     // 'Recipe[]': cho Typescript biết đây là 1 mảng các obj Recipe
 	private recipes: Recipe[] = [
-		new Recipe('A Test Recipe 1', 'This is simply a test', 
-			'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
-		new Recipe('A Test Recipe 2', 'This is simply a test', 
-			'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg'),
+		new Recipe(
+            'A Test Recipe 1', 
+            'This is simply a test', 
+            'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20),
+            ]),
+            
+            
+		new Recipe(
+            'A Test Recipe 2', 
+            'This is simply a test', 
+            'https://www.maxpixel.net/static/photo/1x/Mushrooms-Recipe-Kitchen-French-Dish-2459679.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Onion', 10)
+            ]),
 	];
 
     getRecipes() {
